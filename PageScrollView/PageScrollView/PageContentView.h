5c7cc449396388d691223099af0bbeaaa4a4edd5
Dwt <FineexMac@FineEX-LFdeMac-mini.local>
2017-02-06 13:42:44 +0800

    处理左滑pop和普通左滑

--- PageScrollView/PageScrollView/PageContentView.h
+++ PageScrollView/PageScrollView/PageContentView.h
@@ -9,12 +9,16 @@
 #import <UIKit/UIKit.h>
 #import "PageScrollViewDelegate.h"
 #import "TitleView.h"
+#import "SegmentScrollView.h"
 
 @interface PageContentView : UIView
 
 @property(nonatomic, weak)id<ScrollPageViewDelegate> delegate;
-@property(nonatomic, strong)WTCollectionView *collectionView;
+@property(nonatomic, strong, readonly)WTCollectionView *collectionView;
 
+- (instancetype)initWithFrame:(CGRect)frame segmentView:(SegmentScrollView *)segmentView parentViewController:(UIViewController *)parentViewController delegate:(id<ScrollPageViewDelegate>)delegate;
 
+- (void)setContentOffset:(CGFloat)offset animated:(BOOL)animated;
+- (void)reload;
 
 @end
--- PageScrollView/PageScrollView/PageContentView.m
+++ PageScrollView/PageScrollView/PageContentView.m
@@ -8,7 +8,61 @@
 
 #import "PageContentView.h"
 
+@interface PageContentView ()<UIScrollViewDelegate, UICollectionViewDelegate, UICollectionViewDataSource>{
+    
+    CGFloat _oldOffset;
+    NSInteger _systemVersion;
+    BOOL _isLoadFirstView;
+}
+
+@property(nonatomic, weak)SegmentScrollView *segmentView;
+@property(nonatomic, strong)WTCollectionView *collectionView;
+@property(nonatomic, strong)UICollectionViewLayout *layout;
+@property(nonatomic, weak)UIViewController *parentViewController;
+@property(nonatomic, assign)NSInteger itemsCount;
+@property(nonatomic, strong)NSMutableDictionary<NSString *, UIViewController<ScrollPageViewChildVcDelegate> *> *childVcDict; //所有子控制器
+@property(nonatomic, strong)UIViewController<ScrollPageViewChildVcDelegate> *currentChildVc;
+
+@property(nonatomic, assign)BOOL changeAnimated;
+@property(nonatomic, assign)NSInteger oldIndex;
+@property(nonatomic, assign)NSInteger currentIndex;
+
+@end
+
 @implementation PageContentView
 
+- (instancetype)initWithFrame:(CGRect)frame segmentView:(SegmentScrollView *)segmentView parentViewController:(UIViewController *)parentViewController delegate:(id<ScrollPageViewDelegate>)delegate{
+    
+    if (self = [super initWithFrame:frame]) {
+        self.delegate = delegate;
+        self.segmentView = segmentView;
+        self.parentViewController = parentViewController;
+        
+        [self commonConfig];
+        [self addNotification];
+    }
+    return self;
+}
 
+- (void)commonConfig{
+    
+    _oldIndex = -1;
+    _currentIndex = 0;
+    _oldOffset = 0.0;
+    _isLoadFirstView = YES;
+    _systemVersion = [[[UIDevice currentDevice]systemVersion] integerValue];
+    
+    if (self.delegate && [self.delegate respondsToSelector:@selector(numberOfChildViewControllers)]) {
+        self.itemsCount = [self.delegate numberOfChildViewControllers];
+    }
+    [self addSubview:self.collectionView];
+    
+    //左滑pop和普通左滑的处理
+    UINavigationController *nav = (UINavigationController *)self.parentViewController.parentViewController;
+    
+}
+
+- (void)addNotification{
+    
+}
 @end
